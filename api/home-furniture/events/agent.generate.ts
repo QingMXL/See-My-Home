25c9c2671db64b13ff57009ca539b90b2c701fda
Qing <qingwang@QingdeMacBook-Pro.local>
2026-09-04 19:56:37 +0800

    fix: consolidate furniture API function

--- api/home-furniture/events/agent.generate.ts
+++
@@ -1,6 +0,0 @@
-import handler from '../../home-furniture.js';
-export { config } from '../../home-furniture.js';
-export default function agentGenerate(request: Parameters<typeof handler>[0], response: Parameters<typeof handler>[1]) {
-  request.query.path = 'events/agent.generate';
-  return handler(request, response);
-}
--- api/home-furniture/events/agent.refine.ts
+++
@@ -1,6 +0,0 @@
-import handler from '../../home-furniture.js';
-export { config } from '../../home-furniture.js';
-export default function agentRefine(request: Parameters<typeof handler>[0], response: Parameters<typeof handler>[1]) {
-  request.query.path = 'events/agent.refine';
-  return handler(request, response);
-}
